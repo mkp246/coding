aaf54b6836620b2ecef60d668e8e46ec1c33d264
mkp246 <mkp246@gmail.com>
2018-04-01 16:40:05 +0530

    prepare for junit 5 test case based approch instead of priting to console and verifying manually for correct output

---
+++ src/test/java/gfg/SampleTest.java
@@ -0,0 +1,12 @@
+package gfg;
+
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+  @Test
+  public void test1() {
+    System.out.println("sample test running in Junit 5");
+  }
+}
