4a62c5dcae7c27fa4327c2edd225f7ce6aec30fd
manoj kumar <manoj.kumar22@microfocus.com>
2019-05-11 02:31:02 +0530

    gfg annotation and a sample use

---
+++ src/main/java/com/mkp/GFG.java
@@ -0,0 +1,22 @@
+package com.mkp;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GFG {
+    String date();
+
+    String algo();
+
+    String variant() default "";
+
+    String url();
+
+    Class[] impls() default {};
+}
