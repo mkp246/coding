c2a6e633ec8bfaf24112fbf75ed0599285000eb0
Manoj Kumar <mkp246@gmail.com>
2019-05-27 00:17:16 +0530

    array rotate and range sum: better description and impl class

--- src/test/java/org/gfg/array/RotateAndQueryRangeSumTest.java
+++ src/test/java/org/gfg/array/RotateAndQueryRangeSumTest.java
@@ -6,11 +6,11 @@ import com.mkp.GFGMethod;
 import org.junit.Assert;
 import org.junit.Test;
 
-@GFG(date = "26-05-2019", topic = "Queries on Left and Right Circular shift on array", url = "/queries-left-right-circular-shift-array/")
+@GFG(date = "26-05-2019", topic = "Queries on Left and Right Circular shift on array", url = "/queries-left-right-circular-shift-array/", impls = RotateAndQueryRangeSum.class)
 public class RotateAndQueryRangeSumTest {
 
     @Test
-    @GFGMethod(date = "26-05-2019", algo = "using prefix sum and tracking rotation")
+    @GFGMethod(date = "26-05-2019", algo = "using prefix sum and tracking total rotation, adjusting query range accordingly")
     public void testGetRangeSum() {
         int[] array = {1, 2, 3, 4, 5};
         RotateAndQueryRangeSum query = new RotateAndQueryRangeSum(array);
