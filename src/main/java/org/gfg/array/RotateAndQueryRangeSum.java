3bbe9bef561920953b0e4d2b73ad57ea6758e993
Manoj Kumar <mkp246@gmail.com>
2019-05-26 18:30:41 +0530

    array rotate and range sum

---
+++ src/main/java/org/gfg/array/RotateAndQueryRangeSum.java
@@ -0,0 +1,31 @@
+package org.gfg.array;
+
+public class RotateAndQueryRangeSum {
+    private int totalRotation = 0;
+    private int[] prefixSum;
+
+    public RotateAndQueryRangeSum(int[] array) {
+        prefixSum = array;
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] += prefixSum[i - 1];
+        }
+    }
+
+    public void rotateLeft(int by) {
+        totalRotation -= by;
+    }
+
+    public void rotateRight(int by) {
+        totalRotation += by;
+    }
+
+    public int getRangeSum(int start, int end) {
+        start = (start - totalRotation + prefixSum.length) % prefixSum.length;
+        end = (end - totalRotation + prefixSum.length) % prefixSum.length;
+        if (start < end) {
+            return prefixSum[end] - (start == 0 ? 0 : prefixSum[start - 1]);
+        } else {
+            return prefixSum[prefixSum.length - 1] - prefixSum[start - 1] + (end == 0 ? 0 : prefixSum[end]);
+        }
+    }
+}
---
+++ src/test/java/org/gfg/array/RotateAndQueryRangeSumTest.java
@@ -0,0 +1,26 @@
+package org.gfg.array;
+
+
+import com.mkp.GFG;
+import com.mkp.GFGMethod;
+import org.junit.Assert;
+import org.junit.Test;
+
+@GFG(date = "26-05-2019", topic = "Queries on Left and Right Circular shift on array", url = "/queries-left-right-circular-shift-array/")
+public class RotateAndQueryRangeSumTest {
+
+    @Test
+    @GFGMethod(date = "26-05-2019", algo = "using prefix sum and tracking rotation")
+    public void testGetRangeSum() {
+        int[] array = {1, 2, 3, 4, 5};
+        RotateAndQueryRangeSum query = new RotateAndQueryRangeSum(array);
+
+        query.rotateRight(3);
+        int sum = query.getRangeSum(0, 2);
+        Assert.assertEquals(12, sum);
+
+        query.rotateLeft(1);
+        sum = query.getRangeSum(1, 4);
+        Assert.assertEquals(11, sum);
+    }
+}
