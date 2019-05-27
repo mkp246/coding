730f7d39b4ea9feb9797337003f8d83ae19ac3d7
Manoj Kumar <mkp246@gmail.com>
2019-05-27 22:57:15 +0530

    max 1s length in binary string after k substr rotation

---
+++ src/main/java/org/gfg/StringRotation.java
@@ -0,0 +1,31 @@
+package org.gfg;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class StringRotation {
+
+    public static int maxContiguous1sInBinaryStringAfterSubStringRotations(String string, int rotateTimes) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int count;
+        for (int i = 0; i < string.length(); ) {
+            if (string.charAt(i) == '1') {
+                count = 0;
+                while (i < string.length() && string.charAt(i) == '1') {
+                    count++;
+                    i++;
+                }
+                list.add(count);
+            } else {
+                i++;
+            }
+        }
+
+        list.sort(Comparator.reverseOrder());
+        int result = 0;
+        for (int i = 0; i <= rotateTimes && i < list.size(); i++) {
+            result += list.get(i);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/org/gfg/StringRotationTest.java
@@ -0,0 +1,32 @@
+package org.gfg;
+
+import com.mkp.GFG;
+import com.mkp.GFGMethod;
+import org.junit.Assert;
+import org.junit.Test;
+
+@GFG(date = "27-05-2019", topic = "string rotation", url = "")
+public class StringRotationTest {
+
+    @GFGMethod(date = "27-05-2019", desc = "max continuous 1s in binary string after k substr rotation",
+            algo = "note length of all 1s chunk, sort and sum highest k or all if k > number of 1s chunks",
+            url = "/maximum-contiguous-1-possible-in-a-binary-string-after-k-rotations/")
+    @Test
+    public void testMaxContiguous1sInBinaryStringAfterSubStringRotations() {
+        String string = "100110111";
+        int result = StringRotation.maxContiguous1sInBinaryStringAfterSubStringRotations(string, 1);
+        Assert.assertEquals(5, result);
+
+        string = "100011001";
+        result = StringRotation.maxContiguous1sInBinaryStringAfterSubStringRotations(string, 1);
+        Assert.assertEquals(3, result);
+
+        string = "001100111000110011100";
+        result = StringRotation.maxContiguous1sInBinaryStringAfterSubStringRotations(string, 2);
+        Assert.assertEquals(8, result);
+
+        string = "10011110011";
+        result = StringRotation.maxContiguous1sInBinaryStringAfterSubStringRotations(string, 1);
+        Assert.assertEquals(6, result);
+    }
+}
