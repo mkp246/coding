2039aa445117b22d326db75c55fc55fb915063a4
Manoj Kumar <mkp246@gmail.com>
2019-07-11 17:57:20 +0530

    find element that occurs only once in array

---
+++ src/main/java/org/gfg/array/Occurance.java
@@ -0,0 +1,72 @@
+package org.gfg.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Occurance {
+    public static int[] findOccuredOnceUsingSorting(int[] array) {
+        Arrays.sort(array);
+        List<Integer> result = new ArrayList<>();
+        int index = 0;
+        //first and last needs to have special check to prevent out of bound index 
+        if (array[index] != array[index + 1]) result.add(array[index]);
+        for (index = 1; index < array.length - 1; index++) {
+            if (array[index - 1] != array[index] && array[index] != array[index + 1]) result.add(array[index]);
+        }
+        if (array[index - 1] != array[index]) result.add(array[index]);
+
+        int[] resultArray = new int[result.size()];
+        index = 0;
+        for (int number : result) {
+            resultArray[index++] = number;
+        }
+        return resultArray;
+    }
+
+    public static int[] findOccuredOnceUsingHashing(int[] array) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int element : array) {
+            if (map.containsKey(element)) {
+                map.put(element, map.get(element) + 1);
+            } else {
+                map.put(element, 1);
+            }
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        for (Map.Entry entry : map.entrySet()) {
+            if ((int) entry.getValue() == 1) {
+                result.add((Integer) entry.getKey());
+            }
+        }
+
+        return intListToArray(result);
+    }
+
+    private static int[] intListToArray(ArrayList<Integer> result) {
+        int[] resultArray = new int[result.size()];
+        int index = 0;
+        for (int number : result) {
+            resultArray[index++] = number;
+        }
+        return resultArray;
+    }
+
+    public static int[] findOccuredOnceUsingIfDuplicatesAreTogether(int[] array) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                result.add(array[i]);
+                break;
+            }
+            if (array[i] == array[i + 1]) {
+                while (i + 1 < array.length && array[i + 1] == array[i]) i++;
+            } else {
+                result.add(array[i]);
+            }
+        }
+        return intListToArray(result);
+    }
+}
---
+++ src/test/java/org/gfg/array/OccuranceTest.java
@@ -0,0 +1,41 @@
+package org.gfg.array;
+
+import com.mkp.GFG;
+import com.mkp.GFGMethod;
+import org.junit.Assert;
+import org.junit.Test;
+
+@GFG(date = "11-07-2019", topic = "occurance of element in array", url = "/elements-that-occurred-only-once-in-the-array/", impls = Occurance.class)
+public class OccuranceTest {
+
+    @Test
+    @GFGMethod(date = "11-07-2019", algo = "using soring checking if number not same as prev or next", tComp = "O(nlogn)")
+    public void testFindOccuredOnceUsingSorting() {
+        int[] array = {7, 7, 8, 8, 9, 1, 1, 4, 2, 2};
+        int[] result = Occurance.findOccuredOnceUsingSorting(array);
+        Assert.assertArrayEquals(new int[]{4, 9}, result);
+    }
+
+    @Test
+    @GFGMethod(date = "11-07-2019", algo = "using hashing")
+    public void testFindOccuredOnceUsingHashing() {
+        int[] array = {7, 7, 8, 10, 9, 1, 1, 4, 2, 2};
+        int[] result = Occurance.findOccuredOnceUsingHashing(array);
+        Assert.assertArrayEquals(new int[]{4, 8, 9, 10}, result);
+    }
+
+    @Test
+    @GFGMethod(date = "11-07-2019", algo = "using skipping duplicates")
+    public void testFindOccuredOnceUsingIfDuplicatesAreTogether() {
+        int[] array = {7, 7, 8, 10, 9, 1, 1, 4, 2, 2, 11};
+        int[] result = Occurance.findOccuredOnceUsingIfDuplicatesAreTogether(array);
+        Assert.assertArrayEquals(new int[]{8, 10, 9, 4, 11}, result);
+
+        array = new int[]{7, 7, 7};
+        result = Occurance.findOccuredOnceUsingIfDuplicatesAreTogether(array);
+        Assert.assertArrayEquals(new int[]{}, result);
+        array = new int[]{7, 7, 8};
+        result = Occurance.findOccuredOnceUsingIfDuplicatesAreTogether(array);
+        Assert.assertArrayEquals(new int[]{8}, result);
+    }
+}
