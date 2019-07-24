17ee8683c2488c74bb4da5da0246b39dfef3b9de
Manoj Kumar <mkp246@gmail.com>
2019-07-24 15:34:22 +0530

    kth smallest -using sorting -using quick sort partition

---
+++ src/main/java/org/gfg/array/OrderStatistics.java
@@ -0,0 +1,35 @@
+package org.gfg.array;
+
+import java.util.Arrays;
+
+public class OrderStatistics {
+    public static int getKthSmallestNumberUsingSorting(int[] array, int k) {
+        Arrays.sort(array);
+        return array[k - 1];
+    }
+
+    public static int getKthSmallestNumberUsingPartition(int[] array, int start, int end, int k) {
+        if (end - start + 1 >= k) {
+            int pos = partition(array, start, end);
+            if (pos - start == k - 1) {
+                return array[pos];
+            } else if (pos - start > k - 1) {
+                return getKthSmallestNumberUsingPartition(array, start, pos - 1, k);
+            } else {
+                return getKthSmallestNumberUsingPartition(array, pos + 1, end, k - pos + start - 1);
+            }
+        }
+        return 0;
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        for (int i = start; i < end; i++) {
+            if (array[i] <= pivot) {
+                ArrayRearrange.swapIndex(array, i, start++);
+            }
+        }
+        ArrayRearrange.swapIndex(array, start, end);
+        return start;
+    }
+}
---
+++ src/test/java/org/gfg/array/OrderStatisticsTest.java
@@ -0,0 +1,30 @@
+package org.gfg.array;
+
+import com.mkp.GFG;
+import com.mkp.GFGMethod;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@GFG(date = "24-07-2019", topic = " array order statistics", url = "/array-data-structure/array-order-statistics/")
+public class OrderStatisticsTest {
+
+    @Test
+    @GFGMethod(date = "24-07-2019", algo = "(method 1)sort and get return array[k-1]", url = "kth-smallestlargest-element-unsorted-array/")
+    public void testGetKthSmallestNumberUsingSorting() {
+        int[] array = {7, 10, 4, 3, 20, 15};
+        int result = OrderStatistics.getKthSmallestNumberUsingSorting(array, 3);
+        assertEquals(7, result);
+
+        result = OrderStatistics.getKthSmallestNumberUsingSorting(array, 4);
+        assertEquals(10, result);
+    }
+
+    @Test
+    @GFGMethod(date = "24-07-2019", algo = "(method 4)quick sort partition around last element", url = "kth-smallestlargest-element-unsorted-array/", tComp = "avg O(n), worst O(n^2)")
+    public void testGetKthSmallestNumberUsingPartition() {
+        int[] array = {12, 3, 5, 7, 4, 19, 26};
+        int result = OrderStatistics.getKthSmallestNumberUsingPartition(array, 0, array.length - 1, 3);
+        assertEquals(5, result);
+    }
+}
