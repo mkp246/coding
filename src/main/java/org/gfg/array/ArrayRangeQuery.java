e20e669e52c868083cc3b6880b15c90312f7fa0d
Manoj Kumar <mkp246@gmail.com>
2019-08-03 23:04:02 +0530

    create array range query files and unicode display github test

---
+++ src/main/java/org/gfg/array/ArrayRangeQuery.java
@@ -0,0 +1,8 @@
+package org.gfg.array;
+
+public class ArrayRangeQuery {
+
+    public static int[] findRangeSumUsingMOAlgorithm(int[] array, int[][] query) {
+        return null;
+    }
+}
---
+++ src/test/java/org/gfg/array/ArrayRangeQueryTest.java
@@ -0,0 +1,18 @@
+package org.gfg.array;
+
+import com.mkp.GFG;
+import com.mkp.GFGMethod;
+import org.junit.Test;
+
+@GFG(date = "03-08-2019", topic = "array range queries", url = "/array-data-structure/array-range-queries/", impls = ArrayRangeQuery.class)
+public class ArrayRangeQueryTest {
+
+    @Test
+    @GFGMethod(date = "03-08-2019", algo = "query square root decomposition (MO's algorithm)",
+            url = "/mos-algorithm-query-square-root-decomposition-set-1-introduction/", tComp = "O((m+n)√n)")
+    public void testFindRangeSumUsingMOAlgorithm() {
+        int[] array = {1, 1, 2, 1, 3, 4, 5, 2, 8};
+        int[][] query = {{0, 4}, {1, 3}, {2, 4}};
+        int[] result = ArrayRangeQuery.findRangeSumUsingMOAlgorithm(array, query);
+    }
+}
