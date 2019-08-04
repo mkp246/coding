bf6e1013e5ccb00ba75baa767bc0a8dafb2a12d5
Manoj Kumar <mkp246@gmail.com>
2019-08-04 12:33:12 +0530

    array range query using square root decomposition MO's Algorithm

--- src/main/java/org/gfg/array/ArrayRangeQuery.java
+++ src/main/java/org/gfg/array/ArrayRangeQuery.java
@@ -1,8 +1,41 @@
 package org.gfg.array;
 
+import java.util.Arrays;
+
 public class ArrayRangeQuery {
 
-    public static int[] findRangeSumUsingMOAlgorithm(int[] array, int[][] query) {
-        return null;
+    public static int[][] findRangeSumUsingMOAlgorithm(int[] array, int[][] query) {
+        double blockSize = Math.sqrt(array.length);
+        Arrays.sort(query, (o1, o2) -> {
+            if ((int) (o1[0] / blockSize) == (int) (o2[0] / blockSize)) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int[][] result = new int[query.length][3];
+        int resultPos = 0;
+        int currL = 0, currR = 0, currSum = array[0];
+        for (int[] element : query) {
+            while (currL < element[0]) {
+                currSum -= array[currL++];
+            }
+            while (currL > element[0]) {
+                currSum += array[--currL];
+            }
+
+            while (currR < element[1]) {
+                currSum += array[++currR];
+            }
+            while (currR > element[1]) {
+                currSum -= array[currR--];
+            }
+            result[resultPos][0] = element[0];
+            result[resultPos][1] = element[1];
+            result[resultPos++][2] = currSum;
+        }
+
+        return result;
     }
 }
--- src/test/java/org/gfg/array/ArrayRangeQueryTest.java
+++ src/test/java/org/gfg/array/ArrayRangeQueryTest.java
@@ -2,6 +2,9 @@ package org.gfg.array;
 
 import com.mkp.GFG;
 import com.mkp.GFGMethod;
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 @GFG(date = "03-08-2019", topic = "array range queries", url = "/array-data-structure/array-range-queries/", impls = ArrayRangeQuery.class)
@@ -12,7 +15,8 @@ public class ArrayRangeQueryTest {
             url = "/mos-algorithm-query-square-root-decomposition-set-1-introduction/", tComp = "O((m+n)sqrt(n))")
     public void testFindRangeSumUsingMOAlgorithm() {
         int[] array = {1, 1, 2, 1, 3, 4, 5, 2, 8};
-        int[][] query = {{0, 4}, {1, 3}, {2, 4}};
-        int[] result = ArrayRangeQuery.findRangeSumUsingMOAlgorithm(array, query);
+        int[][] query = {{0, 4}, {1, 3}, {7, 8}, {4, 6}, {6, 8}, {8, 8}, {0, 5}, {3, 5}, {2, 4}};
+        int[][] result = ArrayRangeQuery.findRangeSumUsingMOAlgorithm(array, query);
+        assertArrayEquals(new int[][]{{1, 3, 4}, {0, 4, 8}, {2, 4, 6}, {0, 5, 12}, {3, 5, 8}, {4, 6, 12}, {7, 8, 10}, {6, 8, 15}, {8, 8, 8}}, result);
     }
 }
